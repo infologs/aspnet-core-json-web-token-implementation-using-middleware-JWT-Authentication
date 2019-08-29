8ede55a780c225807fc48780949bff03e22614ea
Example Store <webboostings@gmail.com>
2019-08-29 08:40:55 +0530

    Added Controller and Settings files

---
+++ JWTTokenProject/Controllers/ValuesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JWTTokenProject.Controllers
+{
+    public class TokenProviderOptions
+    {
+        public string Path { get; set; } = "/token";
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public TimeSpan Expiration { get; set; } = TimeSpan.FromMinutes(5);
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+
+    public class TokenProviderMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly TokenProviderOptions _options;
+
+        public TokenProviderMiddleware(RequestDelegate next, IOptions<TokenProviderOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            // If the request path doesn't match, skip
+            if (!context.Request.Path.Equals(_options.Path, StringComparison.Ordinal))
+            {
+                return _next(context);
+            }
+
+            // Request must be POST with Content-Type: application/x-www-form-urlencoded
+            if (!context.Request.Method.Equals("POST"))
+            {
+                context.Response.StatusCode = 400;
+                return context.Response.WriteAsync("Bad request.");
+            }
+
+            return GenerateToken(context);
+        }
+
+        private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            username = "TEST";
+            password = "TEST123";
+            // DON'T do this in production, obviously!
+            if (username == "TEST" && password == "TEST123")
+            {
+                return Task.FromResult(new ClaimsIdentity(new System.Security.Principal.GenericIdentity(username, "Token"), new Claim[] { new Claim("sub", "Test", "username", "infologs.in", "infologs.in"),
+                new Claim("jti", Guid.NewGuid().ToString()),
+                new Claim("iat", DateTime.Now.ToUniversalTime().ToString(), ClaimValueTypes.Integer) }));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+
+
+        private async Task GenerateToken(HttpContext context)
+        {
+            var username = context.Request.Form["username"];
+            var password = context.Request.Form["password"];
+
+            var identity = await GetIdentity(username, password);
+            if (identity == null)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            try
+            {
+
+                var now = DateTime.UtcNow;
+
+                // Specifically add the jti (random nonce), iat (issued timestamp), and sub (subject/user) claims.
+                // You can add other claims here, if you want:
+                //var claims = new Claim[]
+                //{
+                //    new Claim(JwtRegisteredClaimNames.),
+                //    new Claim(JwtRegisteredClaimNames.Sub, username)
+                //};
+
+                var claims = new List<Claim>
+                {
+                    new Claim("sub", "Test", "username", "infologs.in", "infologs.in", identity)
+                };
+
+                // Create the JWT and write it to a string
+                var jwt = new JwtSecurityToken(
+                    issuer: _options.Issuer,
+                    audience: _options.Audience,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(_options.Expiration),
+                    signingCredentials: _options.SigningCredentials);
+
+                var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+                var response = new
+                {
+                    access_token = encodedJwt,
+                    expires_in = (int)_options.Expiration.TotalSeconds
+                };
+
+                // Serialize and return the response
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+
+
+
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
